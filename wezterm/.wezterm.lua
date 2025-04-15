a890aacf030a3ca642bbe07c595b035417331430
Danil Khismatulin <danil@khismatulin.com>
2025-04-15 09:41:49 +0200

    Wezterm config added

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+
+return {
+    color_scheme = "Catppuccin Frappe",
+    window_decorations = "NONE",
+    initial_cols = 130,
+    initial_rows = 33,
+    font_size = 10.0,
+    default_prog = { "nu", "-l" },
+  }
